d84610ca247f7b9009dca352d35da0b3ae828085
Victor Teppeh <victorteppeh233@gmail.com>
2025-09-16 19:27:17 +0000

    changing the case

---
+++ chapter02/06_name.py
@@ -0,0 +1,12 @@
+# Changing Case in a String with Methods
+
+name= "ada lovelace"
+
+# capitalizes only the first letter of each word
+print(name.title())
+
+#changes a string to all uppercase
+print(name.upper())
+
+#changes a string to all lowercase
+print(name.lower())
