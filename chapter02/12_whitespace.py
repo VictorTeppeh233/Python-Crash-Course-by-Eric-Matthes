f85050fdadfe669cdc1e750f6698e303cb737b7a
Victor Teppeh <victorteppeh233@gmail.com>
2025-09-16 19:34:54 +0000

    removing whitespaces

---
+++ chapter02/12_whitespace.py
@@ -0,0 +1,11 @@
+#removing whitespace
+
+
+favorite_language = " python "
+
+#removing whitespaces from both right and left
+
+favorite_language = favorite_language.strip()
+
+#showing it
+print(favorite_language)
