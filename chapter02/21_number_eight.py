80d7e4cd636f4ffdd8279ce172ca42d52ff825bb
Victor Teppeh <victorteppeh233@gmail.com>
2025-09-16 19:49:13 +0000

    assignment 2-9

---
+++ chapter02/21_number_eight.py
@@ -0,0 +1,12 @@
+#Assignment 2-9
+
+#Write addition, subtraction, multiplication, and division 
+#operations that each result in the number 8
+
+print(4 + 4)
+
+print(10 - 2)
+
+print(4 * 2)
+
+print(80 / 10)
