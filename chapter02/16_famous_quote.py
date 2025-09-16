70a0f5feb305f892fefd5a18ce75ffe2b6e243c3
Victor Teppeh <victorteppeh233@gmail.com>
2025-09-16 19:44:10 +0000

    assignment 2-5

---
+++ chapter02/16_famous_quote.py
@@ -0,0 +1,10 @@
+# Assignment 2-5: Famous Quote
+#Find a quote from a famous person you admire.
+# Print the quote and the name of its author
+
+#assigning the quote and author to variables
+author = "Linus Torvalds"
+quote = "Talk is cheap. Show me the code."
+
+#printing the quote and author
+print(f"{author} once said, '{quote}' ")
