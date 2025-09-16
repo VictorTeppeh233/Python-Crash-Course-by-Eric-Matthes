ac9bee04180972b3ce3da9f4e0bdf473eb744309
Victor Teppeh <victorteppeh233@gmail.com>
2025-09-16 19:36:26 +0000

    Assignment 2.3

---
+++ chapter02/14_personal_message.py
@@ -0,0 +1,9 @@
+#Assignment 2.3
+
+#Use a variable to represent a person’s name, and print a message to that person.
+
+#Assigning the name to a variable
+name = "Naomi"
+
+#printing a message to her
+print(f"Hello {name}, would you like to learn Python today?")
