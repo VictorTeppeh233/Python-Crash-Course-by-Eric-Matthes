6f0506929738407e9064b17d32b08c2cff3afd2f
Victor Teppeh <victorteppeh233@gmail.com>
2025-09-16 19:28:39 +0000

    combining the fullname

---
+++ chapter02/09_full_name.py
@@ -0,0 +1,14 @@
+# Using Variables in Strings
+
+#assign firstname and lastname
+first_name = "ada"
+last_name = "lovelace"
+
+#combine them
+full_name = f"{first_name} {last_name}"
+
+#assign the fullname to the variable "message"
+message = f"Hello, {full_name.title()}!"
+
+#print the message
+print(message)
