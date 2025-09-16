41c080a4297329b0acf8934ce700477332189836
Victor Teppeh <victorteppeh233@gmail.com>
2025-09-16 19:49:45 +0000

    assignment 2-10

---
+++ chapter02/22_favorite_number.py
@@ -0,0 +1,11 @@
+#Assigment 2-10
+
+#Use a variable to represent your favorite number. 
+#Then using that variable, create a message that reveals your favorite number.
+#Print that message.
+
+#Assigning the number to the variable
+fav_number = 1957
+
+#printing the number in a sentence
+print(f"My favorite number is {fav_number}")
