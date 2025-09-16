c687775234504b9fe0c8d04e68f690c7b6ae283b
Victor Teppeh <victorteppeh233@gmail.com>
2025-09-16 19:46:50 +0000

    removing extensions from a filename

---
+++ chapter02/19_file_extension.py
@@ -0,0 +1,11 @@
+#Assign the value 'python_notes.txt' to a variable called filename.
+#Then use the removesuffix() method to display the filename withoutthe file extension
+
+#assigning 'python_notes.txt' to filename
+filename = 'python_notes.txt'
+
+#removing the suffix
+filename = filename.removesuffix(".txt")
+
+#print the filename
+print(filename)
