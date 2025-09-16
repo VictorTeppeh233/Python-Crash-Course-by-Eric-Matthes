48027fa60cf9d19d57ba2ebc341575579dddad8b
Victor Teppeh <victorteppeh233@gmail.com>
2025-09-16 19:43:37 +0000

    assignment 2-4

---
+++ chapter02/15_name_cases.py
@@ -0,0 +1,16 @@
+#Assignment 2.4
+
+#Use a variable to represent a person’s name, and then  
+#print that person’s name in lowercase, uppercase, and title case
+
+#assigning the name to a variable
+name = "John Kwame Doe"
+
+#lowercase
+print(name.lower())
+
+#uppercase
+print(name.upper())
+
+#title case
+print(name.title())
