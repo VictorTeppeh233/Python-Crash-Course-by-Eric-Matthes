62e36c4a61a3cb1b12a1cbd8e500398992305e52
Victor Teppeh <victorteppeh233@gmail.com>
2025-09-16 19:48:31 +0000

    playing with numbers data types

---
+++ chapter02/20_numbers.py
@@ -0,0 +1,15 @@
+# A brief overview of numbers data types
+
+#assigning values to variables
+a, b, c, x, y, z = 5, 10, 50, 2.5, 5.5, 20.5
+
+print(a + b)
+
+print(a + z)
+
+print(b / a)
+
+print(a * c)
+
+print(b ** 2)
+      
