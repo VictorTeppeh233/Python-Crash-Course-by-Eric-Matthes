393a1502676e26148f3de4786ba3b26e57a2d09e
Victor Teppeh <victorteppeh233@gmail.com>
2025-09-16 19:25:32 +0000

    printing a simple message

---
+++ chapter02/04_simple_messages.py
@@ -0,0 +1,9 @@
+# assigning a message to a variable, then printing it
+
+message = "I love Python"
+print(message)
+
+#change the value of the message then print again
+
+message = "I love Ghana"
+print(message)
