f522bbb7fdf3117c5e1a5b281d62a9dbf3b54018
Victor Teppeh <victorteppeh233@gmail.com>
2025-09-18 10:37:29 +0000

    formatting elements in a list

---
+++ chapter03/03-bicycles.py
@@ -0,0 +1,9 @@
+# Formatting Elements in a List
+
+# creating the list
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+
+# pulling out the first bicycle in the list
+# formatting it when printing
+print(bicycles[0].title())
+
