d873354e3cfca0eefe29b03854eabf3c4536861c
Victor Teppeh <victorteppeh233@gmail.com>
2025-09-18 10:36:16 +0000

    accessing elements in a list

---
+++ chapter03/02-bicycles.py
@@ -0,0 +1,9 @@
+# Accessing Elements in a List
+
+# creating the list
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+
+# pulling out the first bicycle in the list
+# lists start from index 0
+print(bicycles[0])
+
