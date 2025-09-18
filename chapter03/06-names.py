12ae86dc9857d6af2a0e60feb511ae26b64972ce
Victor Teppeh <victorteppeh233@gmail.com>
2025-09-18 11:02:39 +0000

    printing names of friends

---
+++ chapter03/06-names.py
@@ -0,0 +1,9 @@
+# Assignment 3-1 Names
+# Store names of a few friends in a list called names.
+# Print each person's name by accessing each element in the list, one at a time
+
+# Storing the names
+names = ["Felicity", "Samuel", "Cecilia", "John", "Hannah"]
+
+# printing the names
+print(f"{names[0]}\n{names[1]}\n{names[2]}\n{names[3]}\n{names[4]}\n")
