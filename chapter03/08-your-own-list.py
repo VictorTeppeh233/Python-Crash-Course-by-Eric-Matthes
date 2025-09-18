fe4b750e7cd409fb23845bc761ae0bbd3a6ac48c
Victor Teppeh <victorteppeh233@gmail.com>
2025-09-18 11:31:02 +0000

    made sentences with my own list

---
+++ chapter03/08-your-own-list.py
@@ -0,0 +1,14 @@
+# Assignment 3-3 Your own list
+# make a list of your mode of transportation
+transportations = ["bus", "motor", "taxi", "helicopter", "private jet"]
+
+#using the elements to form sentences
+print(f"Taking the {transportations[0]} is the cheapest mode of transportation.")
+
+print(f"I once rode a {transportations[1]} to a tech event.")
+
+print(f"I usually take a {transportations[2]} when I'm running late.")
+
+print(f"I once travelled in a {transportations[3]} to Tamale.")
+
+print(f"Most billionaires own a {transportations[4]}.")
