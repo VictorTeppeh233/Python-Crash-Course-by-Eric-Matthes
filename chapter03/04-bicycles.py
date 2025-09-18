4a5f66449cf7600f373def9dd8798c4247f08ad8
Victor Teppeh <victorteppeh233@gmail.com>
2025-09-18 10:39:40 +0000

    printing specific elements

---
+++ chapter03/04-bicycles.py
@@ -0,0 +1,11 @@
+# Index positions of a list
+
+# creating the list
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+
+# printing the list
+print(bicycles[1])
+print(bicycles[2])
+
+#printing the last element
+print(bicycles[-1])
