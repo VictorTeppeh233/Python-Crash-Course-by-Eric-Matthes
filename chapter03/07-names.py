b2a5b6f7431063a6cdf7788a8b58058e8b968708
Victor Teppeh <victorteppeh233@gmail.com>
2025-09-18 11:03:45 +0000

    printing personalized message to friends

---
+++ chapter03/07-names.py
@@ -0,0 +1,13 @@
+# Assignment 3-2 Greetings
+# Store names of a few friends in a list called names.
+# Print print a personlized message to them.
+
+# Storing the names
+names = ["Felicity", "Samuel", "Cecilia", "John", "Hannah"]
+
+# printing the names with the messages
+print(f"Hello {names[0]}, trust you're doing well\n")
+print(f"Hello {names[1]}, trust you're doing well\n")
+print(f"Hello {names[2]}, trust you're doing well\n")
+print(f"Hello {names[3]}, trust you're doing well\n")
+print(f"Hello {names[4]}, trust you're doing well\n")
