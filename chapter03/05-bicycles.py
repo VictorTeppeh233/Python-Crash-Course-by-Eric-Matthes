6e89a2c89fb62411efb1e9c050ee3e661b779c0d
Victor Teppeh <victorteppeh233@gmail.com>
2025-09-18 10:41:45 +0000

    forming sentence with elements in a list

---
+++ chapter03/05-bicycles.py
@@ -0,0 +1,10 @@
+# Using individual values from a list
+
+# creating the list
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+
+
+#using the first element to form a sentence
+message = (f"My first bicycle was a {bicycles[0].title()}.")
+
+print(message)
