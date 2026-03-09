521ace6fc48a813a355970c71dbe27260aa433c4
Victor Teppeh <victorteppeh233@gmail.com>
2026-03-09 23:02:42 +0000

    working with removed items from a list

---
+++ chapter03/17-motorcycles.py
@@ -0,0 +1,8 @@
+#working with a value that is removed from a list
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print(motorcycles)
+
+too_expensive = 'ducati'
+motorcycles.remove(too_expensive)
+print(motorcycles)
+print(f"\n A {too_expensive.title()} is too expensive for me.")
