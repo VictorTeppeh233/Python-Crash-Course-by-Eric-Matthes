c57f8a1128a55895630c4a782bcae5fa498fabe1
Victor Teppeh <victorteppeh233@gmail.com>
2026-03-09 22:51:58 +0000

    popping an item from any position in a list

---
+++ chapter03/15-motorcycles.py
@@ -0,0 +1,6 @@
+#popping items from any position in a list
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+first_owned = motorcycles.pop(0)
+print(f" The first motorcycle I owned was a {first_owned.title()}.")
