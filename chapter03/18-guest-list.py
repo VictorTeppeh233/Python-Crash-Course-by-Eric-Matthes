a431973982c04d8eb979ac2ac0e8255b8e6afeb2
Victor Teppeh <victorteppeh233@gmail.com>
2026-03-09 23:13:49 +0000

    assignment 3.4

---
+++ chapter03/18-guest-list.py
@@ -0,0 +1,15 @@
+# Assignment 3.4 
+#Make a list that includes at least three people you’d like to invite to dinner.
+#Then use your list to print a message to each person, inviting them to dinner.
+
+dinner_list = ["Sena", "Samuel", "Felicity", "Karen", "Naomi"]
+
+print(f"Dear {dinner_list[0].title()}, you are warmly invited to the end of year dinner hosted by Victor.")
+
+print(f"Dear {dinner_list[1].title()}, you are warmly invited to the end of year dinner hosted by Victor.")
+
+print(f"Dear {dinner_list[2].title()}, you are warmly invited to the end of year dinner hosted by Victor.")
+
+print(f"Dear {dinner_list[3].title()}, you are warmly invited to the end of year dinner hosted by Victor.")
+
+print(f"Dear {dinner_list[4].title()}, you are warmly invited to the end of year dinner hosted by Victor.")
