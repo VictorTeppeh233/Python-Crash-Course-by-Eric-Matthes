d3597ad404682ce91e0ad3fe06bc444644752559
Victor Teppeh <victorteppeh233@gmail.com>
2026-03-09 22:45:58 +0000

    removing an item using pop method

---
+++ chapter03/14-motorcycles.py
@@ -0,0 +1,7 @@
+#removing an item using the pop() method
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+popped_motorcycle = motorcycles.pop()
+print(motorcycles)
+print(popped_motorcycle)
