5537fc4ea55c670210362e58bd4a36f6bd4bba84
Victor Teppeh <victorteppeh233@gmail.com>
2026-03-09 22:31:18 +0000

    appending items to an empty list

---
+++ chapter03/11-motorcycles.py
@@ -0,0 +1,11 @@
+#creating an empty list
+motorcycles = []
+print(motorcycles)
+
+#appending items to the list
+motorcycles.append("honda")
+motorcycles.append("yamaha")
+motorcycles.append("suzuki")
+
+#priting the final list
+print(motorcycles)
