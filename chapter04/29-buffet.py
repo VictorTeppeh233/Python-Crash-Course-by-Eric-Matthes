ea3016da5c4aa2c6a0ba80e75ff5251b19ef9e57
Victor Teppeh <victorteppeh233@gmail.com>
2026-03-14 17:51:03 +0000

    tuples

--- chapter04/29-buffet.py
+++ chapter04/29-buffet.py
@@ -3,9 +3,9 @@
 #Use a for loop to print each food the restaurant offers.
 #Try to modify one of the items, and make sure that Python rejects the change.
 #The restaurant changes its menu, replacing two of the items with different foods. 
-#Add a line that rewrites the tuple, and then use a for loop to print each of the items on the revised menu.
+    
 
-buffet_menu = ('fufu', 'banku', 'jollof', 'waakye', 'kenkey')
+buffet_menu = ('fufu', 'banku', 'jollof', 'waakye', 'kenkey')#Add a line that rewrites the tuple, and then use a for loop to print each of the items on the revised menu.
 for food in buffet_menu:
     print(food)
     
