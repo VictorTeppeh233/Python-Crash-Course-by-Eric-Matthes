41d8dc126b012579305d1441a9fb1b76b4160564
Victor Teppeh <victorteppeh233@gmail.com>
2026-03-21 00:02:38 +0000

    checking for value in list

---
+++ chapter05/07-toppings.py
@@ -0,0 +1,10 @@
+#Checking Whether a Value Is in a List
+
+print("list of requested_toppings = ['mushrooms', 'onions', 'pineapple']")
+requested_toppings = ['mushrooms', 'onions', 'pineapple']
+
+print('search for mushrooms in list')
+print('mushrooms' in requested_toppings)
+
+print('search for pepperoni')
+print('pepperoni' in requested_toppings)
