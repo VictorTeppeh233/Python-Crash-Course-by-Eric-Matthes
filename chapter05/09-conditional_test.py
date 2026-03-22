b0f6b11327628d5aaccb3cc0dd0b270909f069f2
Victor Teppeh <victorteppeh233@gmail.com>
2026-03-22 23:26:31 +0000

    conditional tests

---
+++ chapter05/09-conditional_test.py
@@ -0,0 +1,14 @@
+#Assignment 5.1
+'''
+Conditional Tests: Write a series of conditional tests. Print a statement
+describing each test and your prediction for the results of each test.
+'''
+
+number = 45
+print("number == 45\n")
+print("Is number = 45? I predict True")
+print(number == 45)
+
+print("\nIs number = 30? I predict False")
+print(number == 30)
+ 
---
+++ chapter05/10-conditional_test.py
@@ -0,0 +1,62 @@
+#Assignment 5.1
+#Create at least 10 tests. 
+#Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+
+days_of_the_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+print("Days of the week\n")
+
+day = "May"
+print(f"Is {day} a day of the  week")
+print(day in days_of_the_week)
+print()
+
+day = "Tuesday"
+print(f"Is {day} a day of the  week")
+print(day in days_of_the_week)
+print()
+
+day = "March"
+print(f"Is {day} a day of the  week")
+print(day in days_of_the_week)
+print()
+
+day = "Sunday"
+print(f"Is {day} a day of the  week")
+print(day in days_of_the_week)
+print()
+
+day = "Friday"
+print(f"Is {day} a day of the  week")
+print(day in days_of_the_week)
+print()
+
+day = "Circle"
+print(f"Is {day} a day of the  week")
+print(day in days_of_the_week)
+print()
+
+day = "Monday"
+print(f"Is {day} a day of the  week")
+print(day in days_of_the_week)
+print()
+
+day = "Bicycle"
+print(f"Is {day} a day of the  week")
+print(day in days_of_the_week)
+print()
+
+day = "Sun"
+print(f"Is {day} a day of the  week")
+print(day in days_of_the_week)
+print()
+
+day = "Saturday"
+print(f"Is {day} a day of the  week")
+print(day in days_of_the_week)
+print()
+
+day = "December"
+print(f"Is {day} a day of the  week")
+print(day in days_of_the_week)
+print()
