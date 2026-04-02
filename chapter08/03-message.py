0619a2daa98f65bce44e782024556dc4da1e63e3
Victor Teppeh <victorteppeh233@gmail.com>
2026-04-02 18:51:04 +0000

    assignments

---
+++ chapter08/03-message.py
@@ -0,0 +1,14 @@
+# assignment 8.1
+
+"""
+Write a function called display_message() that prints one sentence 
+telling everyone what you are learning about in this chapter. 
+Call the function, and make sure the message displays correctly.
+"""
+
+#defining the function
+def display_message():
+    print("Victor is learning about functions.")
+
+#calling the function
+display_message()
