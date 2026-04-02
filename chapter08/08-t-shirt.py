99259e1f9cfd1839ed3ca1ebcb7d8948192189ef
Victor Teppeh <victorteppeh233@gmail.com>
2026-04-02 19:32:54 +0000

    assignment

---
+++ chapter08/08-t-shirt.py
@@ -0,0 +1,16 @@
+# assignment 8.3
+
+"""
+Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. 
+The function should print a sentence summarizing the size of the shirt and the message printed on it.
+Call the function once using positional arguments to make a shirt. 
+Call the function a second time using keyword arguments.
+"""
+
+#defining the function
+def make_shirt(size, message):
+    print(f'Size: {size}')
+    print(f'Message: {message.upper()}.')
+
+make_shirt(18, 'hacktoberfest 2026')
+make_shirt(size=25, message='I love Ghana')
